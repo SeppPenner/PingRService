87604d1a5751898f726aa5cbc1f76fa566f1da40
SeppPenner <HansM2013@gmx.de>
2023-03-15 17:01:28 +0100

    Updated changelog, added Windows binaries, updated build scripts.

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,4 @@
 # Change history
 
+* **Version 1.0.1.0 (2023-03-15)** : Logging only >= warning to Telegram.
 * **Version 1.0.0.0 (2023-03-14)** : 1.0 release.
--- buildAndUploadDockerForArm.bat
+++ buildAndUploadDockerForArm.bat
@@ -1,7 +1,7 @@
 cd src\PingRService
 dotnet publish -c Release --output publish/ -r linux-arm --no-self-contained
-docker build --tag sepppenner/pingrservice-arm:1.0.0 -f Dockerfile.armv7 .
+docker build --tag sepppenner/pingrservice-arm:1.0.1 -f Dockerfile.armv7 .
 docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
-docker push sepppenner/pingrservice-arm:1.0.0
+docker push sepppenner/pingrservice-arm:1.0.1
 @ECHO.Build successful. Press any key to exit.
 pause
