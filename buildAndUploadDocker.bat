6c34c645ecce0a4fad690e001ebcbbd4da6ec7f4
SeppPenner <HansM2013@gmx.de>
2023-04-11 08:31:31 +0200

    Updated build scripts, added Windows binaries.

--- buildAndUploadDocker.bat
+++ buildAndUploadDocker.bat
@@ -1,7 +1,7 @@
 cd src\PingRService
 dotnet publish -c Release --output publish/ -r linux-x64 --no-self-contained
-docker build --tag sepppenner/pingrservice:1.0.1 -f Dockerfile .
+docker build --tag sepppenner/pingrservice:1.0.2 -f Dockerfile .
 docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
-docker push sepppenner/pingrservice:1.0.1
+docker push sepppenner/pingrservice:1.0.2
 @ECHO.Build successful. Press any key to exit.
 pause
