8afe6c9a60772cfe0cba37ba08a6f0ee74b80552
SeppPenner <HansM2013@gmx.de>
2024-01-04 09:02:25 +0100

    Added Windows binaries and updated Docker scripts.

--- buildAndUploadDockerForArm.bat
+++ buildAndUploadDockerForArm.bat
@@ -1,7 +1,7 @@
 cd src\PingRService
 dotnet publish -c Release --output publish/ -r linux-arm --no-self-contained
-docker build --tag sepppenner/pingrservice-arm:1.0.3 -f Dockerfile.armv7 .
+docker build --tag sepppenner/pingrservice-arm:1.0.4 -f Dockerfile.armv7 .
 docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
-docker push sepppenner/pingrservice-arm:1.0.3
+docker push sepppenner/pingrservice-arm:1.0.4
 @ECHO.Build successful. Press any key to exit.
 pause
